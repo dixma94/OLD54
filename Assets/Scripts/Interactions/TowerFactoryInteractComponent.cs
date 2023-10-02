a0b6c7007ba7024456cce5cf0f3953bc169a425e
Astalozar <asgruzdev@gmail.com>
2023-10-02 23:59:46 +0600

    UI update

--- Assets/Scripts/Interactions/TowerFactoryInteractComponent.cs
+++ Assets/Scripts/Interactions/TowerFactoryInteractComponent.cs
@@ -8,6 +8,9 @@ public class TowerFactoryInteractComponent : InteractComponent
 
     public TowerData towerType;
 
+    public TMPro.TextMeshProUGUI woodCost;
+    public TMPro.TextMeshProUGUI rockCost;
+
     public override void Interact(Targettable targettable)
     {
         if (targettable.targetType == EntityType.FactroySlot)
@@ -15,10 +18,19 @@ public class TowerFactoryInteractComponent : InteractComponent
             if (resources.rocksCount >= towerType.rockCost && resources.treesCount >= towerType.woodCost && resources.towerToBuild == 0)
             {
                 resources.towerToBuild = towerType.id;
-                resources.rocksCount--;
-                resources.treesCount--;
+                resources.rocksCount -= towerType.rockCost;
+                resources.treesCount -= towerType.woodCost;
                 resources.UpdateText();
             }
         }
+    }
+
+    private void Update()
+    {
+        woodCost.text = towerType.woodCost.ToString();
+        woodCost.color = resources.treesCount >= towerType.woodCost ? Color.white : Color.red;
+
+        rockCost.text = towerType.rockCost.ToString();
+        rockCost.color = resources.rocksCount >= towerType.rockCost ? Color.white : Color.red;
     }
 }
--- Assets/Scripts/UI/Billboard.cs
+++ Assets/Scripts/UI/Billboard.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
+    Camera m_Camera;
+
+    private void Start()
+    {
+        m_Camera = Camera.main;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
     }
 }
